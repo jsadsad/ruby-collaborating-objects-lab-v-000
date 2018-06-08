88b8abff01a124984544ab5c49045f3262299cc9
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-06-08 01:19:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,8 @@ class Song
     @name = name
   end 
   
-  def self.new_by_filename(file)
-    artist, song = fle.split(" - ")
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
